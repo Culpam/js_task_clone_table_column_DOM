69b89b193ce1bd56b2f663912a2fc00a4d6f28d1
Dmytro Hrabchak <gtcaelumgt@gmail.com>
2025-06-01 01:11:20 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,15 @@
 'use strict';
 
-const thead = document.querySelector('thead');
-const tbody = document.querySelector('tbody');
-const tfoot = document.querySelector('tfoot');
+const table = document.querySelector('table');
+const sections = ['thead', 'tbody', 'tfoot'];
 
-let rows = thead.rows;
+for (const section of sections) {
+  const rows = table.querySelector(section).rows;
+  for (const row of rows) {
+    const cells = row.cells;
+    const clonedCell = cells[1].cloneNode(true);
 
-for (const row of rows) {
-  const cells = row.cells;
-  const clonedCell = cells[1].cloneNode(true);
-
-  cells[cells.length - 1].before(clonedCell);
-}
-
-rows = tbody.rows;
-
-for (const row of rows) {
-  const cells = row.cells;
-  const clonedCell = cells[1].cloneNode(true);
-
-  cells[cells.length - 1].before(clonedCell);
-}
-
-rows = tfoot.rows;
-
-for (const row of rows) {
-  const cells = row.cells;
-  const clonedCell = cells[1].cloneNode(true);
-
-  cells[cells.length - 1].before(clonedCell);
+    const lastCell = cells[cells.length - 1];
+    lastCell.before(clonedCell);
+  }
 }
