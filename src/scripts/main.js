5b5111baab90bdedc664fda5f99260f6b4df621f
Dmytro Hrabchak <gtcaelumgt@gmail.com>
2025-06-01 01:03:02 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write your code here
+const thead = document.querySelector('thead');
+const tbody = document.querySelector('tbody');
+const tfoot = document.querySelector('tfoot');
+
+let rows = thead.rows;
+
+for (const row of rows) {
+  const cells = row.cells;
+  const clonedCell = cells[1].cloneNode(true);
+
+  cells[cells.length - 1].before(clonedCell);
+}
+
+rows = tbody.rows;
+
+for (const row of rows) {
+  const cells = row.cells;
+  const clonedCell = cells[1].cloneNode(true);
+
+  cells[cells.length - 1].before(clonedCell);
+}
+
+rows = tfoot.rows;
+
+for (const row of rows) {
+  const cells = row.cells;
+  const clonedCell = cells[1].cloneNode(true);
+
+  cells[cells.length - 1].before(clonedCell);
+}
