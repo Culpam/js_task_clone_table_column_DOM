bda6b3819901bec1e948a4a8f20957ab138db428
Dmytro Hrabchak <gtcaelumgt@gmail.com>
2025-06-01 01:17:22 +0300

    copy to index 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,11 +5,11 @@ const sections = ['thead', 'tbody', 'tfoot'];
 
 for (const section of sections) {
   const rows = table.querySelector(section).rows;
+
   for (const row of rows) {
     const cells = row.cells;
     const clonedCell = cells[1].cloneNode(true);
 
-    const lastCell = cells[cells.length - 1];
-    lastCell.before(clonedCell);
+    cells[1].after(clonedCell)
   }
 }
